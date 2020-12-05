dffe3094f3a1e2f6a712e867232b8b25f18845f9
Ozkan Sezer <sezeroz@gmail.com>
2020-12-04 20:21:16 -0500

    SDL_sound: clean up the big mess in mpg123 decoder.

--- decoders/mpg123.c
+++ decoders/mpg123.c
@@ -63,7 +63,7 @@ const Sound_DecoderFunctions __Sound_DecoderFunctions_MPG123 =
 {
     {
         extensions_mpg123,
-        "MP3 decoding via internal libmpg123",
+        "MP3 decoding via libmpg123",
         "Ryan C. Gordon <icculus@icculus.org>",
         "http://www.icculus.org/SDL_sound/"
     },
@@ -78,80 +78,34 @@ const Sound_DecoderFunctions __Sound_DecoderFunctions_MPG123 =
 };
 
 
-/* this is what we store in our internal->decoder_private field... */
-typedef mpg123_handle mpg123_t;
-
-static SDL_mutex *mpg123_mutex = NULL;
-static int mpg123_rwops_count = 0;
-static SDL_RWops **mpg123_rwops = NULL;
-
-static void print_decoders(const char *kind, const char **decoders)
-{
-    SNDDBG(("%s:", kind));
-    if (*decoders == NULL)
-        SNDDBG((" [none]"));
-    else
-    {
-        do
-        {
-            SNDDBG((" %s", *decoders));
-        } while (*(++decoders));
-    } /* else */
-    SNDDBG(("\n"));
-} /* print_decoders */
-
+static int mpg123_inited = 0;
 
 static int MPG123_init(void)
 {
-    int retval = 0;
-    assert(mpg123_mutex == NULL);
-    if (mpg123_init() == MPG123_OK)
-    {
-        const char **supported = mpg123_supported_decoders();
-        print_decoders("ALL MPG123 DECODERS", mpg123_decoders());
-        print_decoders("SUPPORTED MPG123 DECODERS", mpg123_supported_decoders());
-        if ((supported != NULL) && (*supported != NULL))
-        {
-            mpg123_mutex = SDL_CreateMutex();
-            if (mpg123_mutex != NULL)
-                retval = 1;  /* at least one decoder available. */
-        } /* if */
-    } /* if */
-
-    return retval;
+    assert(mpg123_inited == 0);
+    mpg123_inited = (mpg123_init() == MPG123_OK);
+    return mpg123_inited;
 } /* MPG123_init */
 
 
 static void MPG123_quit(void)
 {
+    mpg123_inited = 0;
     mpg123_exit();
-    SDL_DestroyMutex(mpg123_mutex);
-    mpg123_mutex = NULL;
-    free(mpg123_rwops);
-    mpg123_rwops = NULL;
-    mpg123_rwops_count = 0;
 } /* MPG123_quit */
 
 
 /* bridge rwops reading to libmpg123 hooks. */
-static ssize_t rwread(int fd, void *buf, size_t len)
+static ssize_t rwread(void *p, void *buf, size_t len)
 {
-    SDL_RWops *rw = NULL;
-    SDL_LockMutex(mpg123_mutex);
-    rw = mpg123_rwops[fd];
-    SDL_UnlockMutex(mpg123_mutex);
-    return (ssize_t) SDL_RWread(rw, buf, 1, len);
+    return (ssize_t) SDL_RWread((SDL_RWops*)p, buf, 1, len);
 } /* rwread */
 
 
 /* bridge rwops seeking to libmpg123 hooks. */
-static off_t rwseek(int fd, off_t pos, int whence)
+static off_t rwseek(void *p, off_t pos, int whence)
 {
-    SDL_RWops *rw = NULL;
-    SDL_LockMutex(mpg123_mutex);
-    rw = mpg123_rwops[fd];
-    SDL_UnlockMutex(mpg123_mutex);
-    return (off_t) SDL_RWseek(rw, pos, whence);
+    return (off_t) SDL_RWseek((SDL_RWops*)p, pos, whence);
 } /* rwseek */
 
 
@@ -166,7 +120,7 @@ static const char *set_error(mpg123_handle *mp, const int err)
 
     snprintf(buffer, sizeof (buffer), "MPG123: %s", str);
     __Sound_SetError(buffer);
-    
+
     return(NULL);  /* this is for BAIL_MACRO to not try to reset the string. */
 } /* set_error */
 
@@ -201,39 +155,15 @@ static int MPG123_open(Sound_Sample *sample, const char *ext)
     int channels = 0;
     int fmt = 0;
     int rc = 0;
-    off_t len = 0;
     int seekable = 0;
-    void *ptr = NULL;
-    int rwops = 0;
-
-    /* !!! FIXME: so much tapdance because we need a pointer, not an int. */
-    SDL_LockMutex(mpg123_mutex);
-    for (rwops = 0; rwops < mpg123_rwops_count; rwops++)
-    {
-        if (mpg123_rwops[rwops] == NULL)
-        break;
-    } /* for */
-    if (rwops < mpg123_rwops_count)
-        ptr = mpg123_rwops;
-    else
-    {
-        mpg123_rwops_count++;
-        ptr = realloc(mpg123_rwops, sizeof (SDL_RWops *) * mpg123_rwops_count);
-        if (ptr != NULL)
-            mpg123_rwops = (SDL_RWops **) ptr;
-    } /* else */
-    if (ptr != NULL)
-        mpg123_rwops[rwops] = internal->rw;
-    SDL_UnlockMutex(mpg123_mutex);
-    BAIL_IF_MACRO(ptr == NULL, ERR_OUT_OF_MEMORY, 0);
 
     if ((mp = mpg123_new(NULL, &rc)) == NULL)
         goto mpg123_open_failed;
     else if ((rc = set_formats(mp)) != MPG123_OK)
         goto mpg123_open_failed;
-    else if ((rc = mpg123_replace_reader(mp, rwread, rwseek)) != MPG123_OK)
+    else if ((rc = mpg123_replace_reader_handle(mp, rwread, rwseek, NULL)) != MPG123_OK)
         goto mpg123_open_failed;
-    else if ((rc = mpg123_open_fd(mp, rwops)) != MPG123_OK)
+    else if ((rc = mpg123_open_handle(mp, internal->rw)) != MPG123_OK)
         goto mpg123_open_failed;
     else if ((rc = mpg123_scan(mp)) != MPG123_OK)
         goto mpg123_open_failed;  /* !!! FIXME: this may be wrong. */
@@ -242,7 +172,6 @@ static int MPG123_open(Sound_Sample *sample, const char *ext)
 
     if (mpg123_seek(mp, 0, SEEK_END) >= 0)  /* can seek? */
     {
-        len = mpg123_tell(mp);
         if ((rc = (int) mpg123_seek(mp, 0, SEEK_SET)) < 0)
             goto mpg123_open_failed;
         seekable = 1;
@@ -281,11 +210,6 @@ static int MPG123_open(Sound_Sample *sample, const char *ext)
     return(1); /* we'll handle this data. */
 
 mpg123_open_failed:
-    SDL_LockMutex(mpg123_mutex);
-    mpg123_rwops[rwops] = NULL;
-    if (rwops == mpg123_rwops_count)
-        mpg123_rwops_count--;
-    SDL_UnlockMutex(mpg123_mutex);
     set_error(mp, rc);
     mpg123_delete(mp);  /* NULL is safe. */
     return(0);
@@ -295,23 +219,7 @@ mpg123_open_failed:
 static void MPG123_close(Sound_Sample *sample)
 {
     Sound_SampleInternal *internal = (Sound_SampleInternal *) sample->opaque;
-    mpg123_t *mp = ((mpg123_t *) internal->decoder_private);
-    int i;
-
-    SDL_LockMutex(mpg123_mutex);
-    for (i = 0; i < mpg123_rwops_count; i++)
-    {
-        if (mpg123_rwops[i] == internal->rw)
-            mpg123_rwops[i] = NULL;
-    } /* for */
-
-    for (i = mpg123_rwops_count-1; i >= 0; i--)
-    {
-        if (mpg123_rwops[i] != NULL)
-            break;
-    } /* for */
-    mpg123_rwops_count = i + 1;
-    SDL_UnlockMutex(mpg123_mutex);
+    mpg123_handle *mp = ((mpg123_handle *) internal->decoder_private);
 
     mpg123_close(mp);  /* don't need this at the moment, but it's safe. */
     mpg123_delete(mp);
@@ -321,7 +229,7 @@ static void MPG123_close(Sound_Sample *sample)
 static Uint32 MPG123_read(Sound_Sample *sample)
 {
     Sound_SampleInternal *internal = (Sound_SampleInternal *) sample->opaque;
-    mpg123_t *mp = ((mpg123_t *) internal->decoder_private);
+    mpg123_handle *mp = ((mpg123_handle *) internal->decoder_private);
     size_t bw = 0;
     const int rc = mpg123_read(mp, (unsigned char *) internal->buffer,
                                internal->buffer_size, &bw);
@@ -340,7 +248,7 @@ static Uint32 MPG123_read(Sound_Sample *sample)
 static int MPG123_rewind(Sound_Sample *sample)
 {
     Sound_SampleInternal *internal = (Sound_SampleInternal *) sample->opaque;
-    mpg123_t *mp = ((mpg123_t *) internal->decoder_private);
+    mpg123_handle *mp = ((mpg123_handle *) internal->decoder_private);
     const int rc = (int) mpg123_seek(mp, 0, SEEK_SET);
     BAIL_IF_MACRO(rc < 0, set_error(mp, rc), 0);
     return(1);
@@ -350,7 +258,7 @@ static int MPG123_rewind(Sound_Sample *sample)
 static int MPG123_seek(Sound_Sample *sample, Uint32 ms)
 {
     Sound_SampleInternal *internal = (Sound_SampleInternal *) sample->opaque;
-    mpg123_t *mp = ((mpg123_t *) internal->decoder_private);
+    mpg123_handle *mp = ((mpg123_handle *) internal->decoder_private);
     const float frames_per_ms = ((float) sample->actual.rate) / 1000.0f;
     const off_t frame_offset = (off_t) (frames_per_ms * ((float) ms));
     const int rc = (int) mpg123_seek(mp, frame_offset , SEEK_SET);
