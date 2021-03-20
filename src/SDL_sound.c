fd0325bf657c1bc461e90e4f5a3a006f88bf8d66
Ozkan Sezer <sezero@users.noreply.github.com>
2021-03-20 14:03:02 +0300

    moved fallback SDL_strtokr to SDL_sound.c

--- src/SDL_sound.c
+++ src/SDL_sound.c
@@ -795,5 +795,66 @@ Sint32 Sound_GetDuration(Sound_Sample *sample)
     return internal->total_time;
 } /* Sound_GetDuration */
 
+
+/* Utility functions ... */
+
+#if !defined(HAVE_SDL_STRTOKR)
+/* Adapted from _PDCLIB_strtok() of PDClib library at
+ * https://github.com/DevSolar/pdclib.git
+ *
+ * The code was under CC0 license:
+ * https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ */
+char *__Sound_strtokr(char *s1, const char *s2, char **ptr)
+{
+    const char *p = s2;
+
+    if (!s2 || !ptr || (!s1 && !*ptr)) return NULL;
+
+    if (s1 != NULL) {  /* new string */
+        *ptr = s1;
+    } else { /* old string continued */
+        if (*ptr == NULL) {
+        /* No old string, no new string, nothing to do */
+            return NULL;
+        }
+        s1 = *ptr;
+    }
+
+    /* skip leading s2 characters */
+    while (*p && *s1) {
+        if (*s1 == *p) {
+        /* found separator; skip and start over */
+            ++s1;
+            p = s2;
+            continue;
+        }
+        ++p;
+    }
+
+    if (! *s1) { /* no more to parse */
+        *ptr = s1;
+        return NULL;
+    }
+
+    /* skipping non-s2 characters */
+    *ptr = s1;
+    while (**ptr) {
+        p = s2;
+        while (*p) {
+            if (**ptr == *p++) {
+            /* found separator; overwrite with '\0', position *ptr, return */
+                *((*ptr)++) = '\0';
+                return s1;
+            }
+        }
+        ++(*ptr);
+    }
+
+    /* parsed to end of string */
+    return s1;
+}
+#endif
+
 /* end of SDL_sound.c ... */
 
