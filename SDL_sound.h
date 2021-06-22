743e6f2b5ee5c9612ca63d3ba5dfdf0c27af5977
Ozkan Sezer <sezero@users.noreply.github.com>
2021-06-22 05:00:28 +0300

    revised the SNDDECLSPEC macro.

--- SDL_sound.h
+++ SDL_sound.h
@@ -73,13 +73,13 @@ extern "C" {
 
 #ifndef DOXYGEN_SHOULD_IGNORE_THIS
 
-#ifndef SDLCALL  /* may not be defined with older SDL releases. */
+#ifndef SDLCALL /* may not be defined with older SDL releases (e.g. < 1.2.4). */
 #define SDLCALL
 #endif
 
-#ifdef SDL_SOUND_DLL_EXPORTS
+#if defined(SDL_SOUND_DLL_EXPORTS) && (defined(_WIN32) || defined(__OS2__))
 #  define SNDDECLSPEC __declspec(dllexport)
-#elif (__GNUC__ >= 3)
+#elif ((defined(__GNUC__) && (__GNUC__ >= 4)) || defined(__clang__)) && !(defined(_WIN32) || defined(__OS2__))
 #  define SNDDECLSPEC __attribute__((visibility("default")))
 #else
 #  define SNDDECLSPEC
