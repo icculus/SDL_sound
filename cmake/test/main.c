bcb02bb1264eb6f471d5f58972bba76f6319b7f8
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-11-08 15:39:23 +0100

    cmake: follow SDL3 conventions

--- cmake/test/main.c
+++ cmake/test/main.c
@@ -2,16 +2,16 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3_sound/SDL_sound.h>
-#include <stdio.h>
 
 int main(int argc, char *argv[]) {
     SDL_SetMainReady();
     if (!SDL_Init(0)) {
-        fprintf(stderr, "SDL_Init: could not initialize SDL: %s\n", SDL_GetError());
+        SDL_Log("SDL_Init failed (%s)", SDL_GetError());
         return 1;
     }
     if (Sound_Init() == 0) {
-        fprintf(stderr, "Sound_Init: failed (%s)\n", Sound_GetError());
+        SDL_Log("Sound_Init failed (%s)", Sound_GetError());
+        return 1;
     }
     Sound_Quit();
     SDL_Quit();
