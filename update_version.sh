64f4b74d80470a7594910f844d20e496825fabfd
Ryan C. Gordon <icculus@icculus.org>
2026-06-07 12:23:55 -0400

    version: Bumped to 3.2.0 for official first SDL3_sound release!

--- update_version.sh
+++ update_version.sh
@@ -6,14 +6,15 @@ if [ -z $1 ]; then
 fi
 
 NEWVERSION="$1"
-echo "Updating version to '3.0.$NEWVERSION' ..."
+echo "Updating version to '3.2.$NEWVERSION' ..."
 
 perl -w -pi -e 's/(set\(MICRO_VERSION )\d+/${1}'$NEWVERSION'/;' CMakeLists.txt
 perl -w -pi -e 's/(\#define SDL_SOUND_MICRO_VERSION )\d+/${1}'$NEWVERSION'/;' include/SDL3_sound/SDL_sound.h
-perl -w -pi -e 's/(FILEVERSION 3,0,)\d+/${1}'$NEWVERSION'/;' src/version.rc
-perl -w -pi -e 's/(PRODUCTVERSION 3,0,)\d+/${1}'$NEWVERSION'/;' src/version.rc
-perl -w -pi -e 's/(VALUE "FileVersion", "3, 0, )\d+/${1}'$NEWVERSION'/;' src/version.rc
-perl -w -pi -e 's/(VALUE "ProductVersion", "3, 0, )\d+/${1}'$NEWVERSION'/;' src/version.rc
+perl -w -pi -e 's/(FILEVERSION 3,2,)\d+/${1}'$NEWVERSION'/;' src/version.rc
+perl -w -pi -e 's/(PRODUCTVERSION 3,2,)\d+/${1}'$NEWVERSION'/;' src/version.rc
+perl -w -pi -e 's/(VALUE "FileVersion", "3, 2, )\d+/${1}'$NEWVERSION'/;' src/version.rc
+perl -w -pi -e 's/(VALUE "ProductVersion", "3, 2, )\d+/${1}'$NEWVERSION'/;' src/version.rc
+perl -w -pi -e 's/(\#define PLAYSOUND_VER_PATCH\s+)\d+/${1}'$NEWVERSION'/;' examples/playsound.c
 
 echo "All done."
 echo "Run 'git diff' and make sure this looks correct before 'git commit'."
