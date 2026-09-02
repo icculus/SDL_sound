libsdl-org/SDL_mixer

readsbk.c: silence READW() pointer sign warnings by adding Uint16* casts

src/timidity/readsbk.c
@@ -277,8 +277,8 @@ static void load_gen(int size, SFInfo *sf, SDL_IOStream *io, int *totalp, tgenre
 	size /= 4;
 	buf = NEW(tgenrec, size);
 	for (i = 0; i < size; i++) {
-		READW(&buf[i].oper, io);
-		READW(&buf[i].amount, io);
+		READW((Uint16 *)&buf[i].oper, io);
+		READW((Uint16 *)&buf[i].amount, io);
 	}
 	*totalp = size;
 	*bufp = buf;

icculus/SDL_sound

src/timidity/readsbk.c
@@ -279,8 +279,8 @@ static void load_gen(int size, SFInfo *sf, SDL_RWops *rw, int *totalp, tgenrec *
 	size /= 4;
 	buf = NEW(tgenrec, size);
 	for (i = 0; i < size; i++) {
-		READW(&buf[i].oper, rw);
-		READW(&buf[i].amount, rw);
+		READW((Uint16 *)&buf[i].oper, rw);
+		READW((Uint16 *)&buf[i].amount, rw);
 	}
 	*totalp = size;
 	*bufp = buf;