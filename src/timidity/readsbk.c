sezero/libtimidity

readsbk.c: make READID and READB macros into functions.
also change the READ?? helpers to return 0 on success.

src/readsbk.c
@@ -51,23 +51,38 @@
 
 static int READCHUNK(tchunk *vp, FILE *fd)
 {
-	if (fread(vp, 8, 1, fd) != 1) return -1;
+	if (fread(vp, 8, 1, fd) != 1) {
+		memset(vp, 0, sizeof(*vp));
+		return -1;
+	}
 	vp->size = SWAPLE32(vp->size);
-	return 1;
+	return 0;
+}
+
+static int READID(char var[4], FILE *fd)
+{
+	if ((fread(var, 4, 1, fd)) != 1) return -1;
+	return 0;
 }
 
 static int READDW(sint32 *vp, FILE *fd)
 {
 	if (fread(vp, 4, 1, fd) != 1) return -1;
 	*vp = SWAPLE32(*vp);
-	return 1;
+	return 0;
 }
 
 static int READW(uint16 *vp, FILE *fd)
 {
 	if (fread(vp, 2, 1, fd) != 1) return -1;
 	*vp = SWAPLE16(*vp);
-	return 1;
+	return 0;
+}
+
+static int READB(uint8 *vp, FILE *fd)
+{
+	if (fread(vp, 1, 1, fd) != 1) return -1;
+	return 0;
 }
 
 static int READSTR(char *str, FILE *fd)
@@ -75,15 +90,13 @@ static int READSTR(char *str, FILE *fd)
 	int n;
 	if (fread(str, 20, 1, fd) != 1) return -1;
 	str[19] = '\0';
-	n = strlen(str);
+	n = (int) strlen(str);
 	while (n > 0 && str[n - 1] == ' ')
 		n--;
 	str[n] = '\0';
-	return n;
+	return 0;
 }
 
-#define READID(var,fd)	fread(var, 1, 4, fd)
-#define READB(var,fd)	fread(var, 1, 1, fd)
 #define SKIPB(fd)	fseek(fd, 1, SEEK_CUR)
 #define SKIPW(fd)	fseek(fd, 2, SEEK_CUR)
 #define SKIPDW(fd)	fseek(fd, 4, SEEK_CUR)

icculus/SDL_sound

src/timidity/readsbk.c
@@ -26,23 +26,38 @@
 
 static int READCHUNK(tchunk *vp, SDL_RWops *rw)
 {
-	if (SDL_RWread(rw, vp, 1, 8) != 8) return -1;
+	if (SDL_RWread(rw, vp, 1, 8) != 8) {
+		SDL_memset(vp, 0, sizeof(*vp));
+		return -1;
+	}
 	vp->size = SDL_SwapLE32(vp->size);
-	return 1;
+	return 0;
+}
+
+static int READID(char var[4], SDL_RWops *rw)
+{
+	if ((SDL_RWread(rw, var, 1, 4)) != 4) return -1;
+	return 0;
 }
 
 static int READDW(Sint32 *vp, SDL_RWops *rw)
 {
 	if (SDL_RWread(rw, vp, 1, 4) != 4) return -1;
 	*vp = SDL_SwapLE32(*vp);
-	return 1;
+	return 0;
 }
 
 static int READW(Uint16 *vp, SDL_RWops *rw)
 {
 	if (SDL_RWread(rw, vp, 1, 2) != 2) return -1;
 	*vp = SDL_SwapLE16(*vp);
-	return 1;
+	return 0;
+}
+
+static int READB(Uint8 *vp, SDL_RWops *rw)
+{
+	if (SDL_RWread(rw, vp, 1, 1) != 1) return -1;
+	return 0;
 }
 
 static int READSTR(char *str, SDL_RWops *rw)
@@ -54,11 +69,9 @@ static int READSTR(char *str, SDL_RWops *rw)
 	while (n > 0 && str[n - 1] == ' ')
 		n--;
 	str[n] = '\0';
-	return n;
+	return 0;
 }
 
-#define READID(var,rw)	SDL_RWread(rw, var, 1, 4)
-#define READB(var,rw)	SDL_RWread(rw, var, 1, 1)
 #define SKIPB(rw)	SDL_RWseek(rw, 1, RW_SEEK_CUR);
 #define SKIPW(rw)	SDL_RWseek(rw, 2, RW_SEEK_CUR);
 #define SKIPDW(rw)	SDL_RWseek(rw, 4, RW_SEEK_CUR);